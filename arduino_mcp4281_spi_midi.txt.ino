876ac6f7e3d4109562945df13c59e81f8423d3e7
Marco Passerini <marco.passerini@gmail.com>
2016-01-16 13:53:16 +0100

    basic lfo forms added, currently buggy

--- arduino_mcp4281_spi_midi.txt.ino
+++ arduino_mcp4281_spi_midi.txt.ino
@@ -26,6 +26,8 @@ float velocity_scaling = 90; // 0-100
 
 // this is double otherwise some operations overflow... check carefully
 double now = 0;
+double now_sec = 0;
+double now_msec = 0; 
 bool legato = true;
 bool first_note = true;
 bool portamento = false;
@@ -36,7 +38,7 @@ StackArray <int> note_stack;
 int vcf = 0;
 int vcf_env_amt = 2048;
 const int vcf_env_amt_max = 4095;
-long env1_attack = 100000;
+long env1_attack = 100000; //milliseconds
 long env1_decay = 1000000;
 int env1_sustain = 2000;
 long env1_release = 2000000;
@@ -51,6 +53,12 @@ int bent_pitch = 0;
 int pitch_bend_cv = 0;
 int pitch_bend_max = 8192;
 int pitch_bend_notes = 12;
+
+int lfo1_type = 0; // 0 = square, 1 = saw, 2 = sine, 3 = random
+float lfo1_rate = 1;
+float lfo1_value = 0;
+float lfo1_amount = 0;
+float lfo1_amount_max = 127;
 
 void setup() {
   Serial.begin(9600);
@@ -72,7 +80,7 @@ void handleNoteOn(byte inChannel, byte inNote, byte inVelocity)
   note_change_time = micros();
   int note_cv = pitch_max / notes_max * (inNote - notes_lowest);
   last_pitch = curr_pitch;
-  if (note_stack.isEmpty()){
+  if (note_stack.isEmpty()) {
     env_trig = true;
     env_trig_time = micros();
   }
@@ -97,14 +105,14 @@ void handleNoteOff(byte inChannel, byte inNote, byte inVelocity)
   }
 }
 
-void handlePitchBend(byte channel, int bend){
-  pitch_bend_cv =  ((long)bend * ((pitch_max / notes_max) * pitch_bend_notes) ) / pitch_bend_max; 
+void handlePitchBend(byte channel, int bend) {
+  pitch_bend_cv =  ((long)bend * ((pitch_max / notes_max) * pitch_bend_notes) ) / pitch_bend_max;
 }
 
-void handleControlChange(byte channel, byte number, byte value){
+void handleControlChange(byte channel, byte number, byte value) {
   switch (number) {
     case 65:
-      
+
       if (value == 0) {
         portamento = false; // portamento and legato  booleans should be fixed, they don't behave correctly
       }
@@ -122,19 +130,32 @@ void handleControlChange(byte channel, byte number, byte value){
       break;
     case 67:
       if (value == 0) {
+        lfo1_type = 0;
+      }
+      else {
+        lfo1_type = 2;
+      }
+      break;
+
+
+    case 68:
+      if (value == 0) {
         pitch_bend_notes = 2;
       }
       else {
         pitch_bend_notes = 12;
       }
       break;
+
     case 81:
       vcf = (float)value / 127 * 4095;
       break;
     case 83:
       vcf_env_amt = (float)value / 127 * 4095; // to be implemented
       break;
-      
+    case 84:
+      lfo1_amount = value;
+      break;
     case 89:
       env1_attack = (float)value / 127 * 1000000;
       break;
@@ -150,7 +171,10 @@ void handleControlChange(byte channel, byte number, byte value){
     case 97:
       slide_time = (float)value / 127 * 1000000;
       break;
-  }  
+    case 98:
+      lfo1_rate = 1 + (float)value;
+      break;
+  }
 }
 
 
@@ -192,10 +216,69 @@ void setOutput(byte channel, byte gain, byte shutdown, unsigned int val)
   PORTB |= 0x4;
 }
 
+void lfo1_square() {
+  if (   ((int)((now_sec) / ((1 / lfo1_rate) / 2) ) % 2) == 0) {
+    lfo1_value = 4095;
+  }
+  else {
+    lfo1_value = 0;
+  }
+}
+
+void lfo1_saw() {
+
+  long period_usec = 1 / (lfo1_rate / 1000000);
+
+  long long y = (long)now % period_usec;
+  lfo1_value = ((float) ((4095 * y) /  (period_usec)) );
+
+
+
+}
+
+void lfo1_triangle() {
+
+  long period_usec = 1 / (lfo1_rate / 1000000);
+  
+  long long y = (long long)now % period_usec;
+ 
+  float value =  ((float) ((4095 * y) /  (period_usec)) );
+
+  if ((((int)floor(now * (lfo1_rate / 1000000))) % 2) == 0) {
+    lfo1_value = value;
+  }
+  else {
+
+    lfo1_value =  4095 - value;
+  }
+}
+
+void lfo1_noise() {
+}
+
+
 void loop() {
   midiA.read();
   //TODO: ADD CHECK FOR OVERFLOW
   now = micros();
+  now_msec = now / 1000;
+  now_sec = now / 1000000;
+
+  switch (lfo1_type) {
+    case 0:
+      lfo1_square();
+      break;
+    case 1:
+      lfo1_saw();
+      break;
+    case 2:
+      lfo1_triangle();
+      break;
+    case 3:
+      lfo1_noise();
+      break;
+  }
+
 
   // PITCH
   if (!note_stack.isEmpty()) {
@@ -213,24 +296,24 @@ void loop() {
   if (!note_stack.isEmpty()) {
 
     // attack section
-    if ((now <= (env_trig_time + env1_attack )) && env_trig ) { 
+    if ((now <= (env_trig_time + env1_attack )) && env_trig ) {
       curr_filter =  (env1_min + (now - env_trig_time) * (env1_max - env1_min) / (env1_attack) )   ;
     }
 
     // decay section
-    else if  (((now >= (env_trig_time + env1_attack )) && (now <= (env_trig_time + env1_decay )) && env_trig)  ) { 
+    else if  (((now >= (env_trig_time + env1_attack )) && (now <= (env_trig_time + env1_decay )) && env_trig)  ) {
       curr_filter = (env1_max + (now - env_trig_time) * (env1_sustain - env1_max) / env1_decay )  ;
     }
     else if ((now > (env_trig_time + env1_attack + env1_decay )) && env_trig )  { // sustain section
       curr_filter =  env1_sustain  ;
-      
+
     }
   }
   else {
     // Release section
     // trick to make it fake exponential... divide the release time by 2, start with a steeper slope
-    if ((now <= (note_off_time + env1_release/2))) { 
-      curr_filter = env_at_note_off + ( now -  note_off_time) * ( env1_min -  env_at_note_off) / (env1_release/2)  ;
+    if ((now <= (note_off_time + env1_release / 2))) {
+      curr_filter = env_at_note_off + ( now -  note_off_time) * ( env1_min -  env_at_note_off) / (env1_release / 2)  ;
       env_at_half_note_off = curr_filter;
     }
     else if ((now <= (note_off_time + env1_release))) { // Release section
@@ -242,7 +325,7 @@ void loop() {
     }
   }
 
-  curr_filter = curr_filter * ((float)vcf_env_amt / vcf_env_amt_max) + vcf;
+  curr_filter = curr_filter * ((float)vcf_env_amt / vcf_env_amt_max) + vcf + lfo1_value * (lfo1_amount / lfo1_amount_max);
 
   if (curr_pitch > pitch_max) {
     curr_pitch = pitch_max;
